35f087f77dd2408dbbf883dc44cb83f10da5d0ca
smile_mylife <843833414@qq.com>
2022-09-21 18:47:04 +0800

    项目初始化

---
+++ shopping-integral/src/main/java/com/example/integral/ShoppingIntegralApplication.java
@@ -0,0 +1,13 @@
+package com.example.integral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingIntegralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingIntegralApplication.class, args);
+    }
+
+}
